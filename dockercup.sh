184ae2180d43a345f8355a83c67463e0cfe7017c
Javier Peletier <jm@epiclabs.io>
2016-04-12 12:23:02 +0200

    * Added --silent flag that can be used when running from cron to avoid passing -it to docker run

--- dockercup.sh
+++ dockercup.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+if [[ "$1" == "--silent" ]]; then
+        FLAGS=""
+        shift
+else
+        FLAGS="-it"
+fi
+
 docker run -it --rm -v /:/host -v /var/run/docker.sock:/docker.sock epiclabs10/dockercup $1
